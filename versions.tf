194a46dc69068dd509343239d82c158bdb8a23ec
RaduLupan <github@lupan.ca>
2021-03-22 07:21:10 -0400

    Updated versions for terraform and aws provider

--- versions.tf
+++ versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.22.0"
+      version = "~> 3.33.0"
     }
   }
-  required_version = ">= 0.13"
+  required_version = ">= 0.14"
 }
