ffa9bbc642d36a026c8b0a2258452f093ab6c69e
RaduLupan <github@lupan.ca>
2021-03-23 08:18:32 -0400

    Created kms key, alias, and key policy from template

--- main.tf
+++ main.tf
@@ -3,7 +3,57 @@ provider "aws" {
 }
 
 locals {
-  s3_name        = "${var.s3_prefix}-${lower(random_string.random.result)}-${var.region}"
+  #iam_user = var.iam_user == null ?         
+  s3_name = "${var.s3_prefix}-${lower(random_string.random.result)}-${var.region}"
+
+  arn_root="arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+  arn_user="arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${var.iam_user}"
+}
+
+# Use this data source to get the access to the effective Account ID, User ID, and ARN in which Terraform is authorized.
+data "aws_caller_identity" "current" {}
+
+# This data source can be used to fetch information about a specific IAM user.
+data "aws_iam_user" "example" {
+  count = var.iam_user == null ? 0 : 1
+
+  user_name = var.iam_user
+}
+
+# Create IAM user if var.iam_user is null.
+resource "aws_iam_user" "admin" {
+  count = var.iam_user == null ? 1 : 0
+
+  name = "mail-export-admin"
+
+  tags = {
+    Name      = "mail-export-admin"
+    terraform = true
+  }
+}
+
+# Template file for the KMS key policy.
+data "template_file" "kms_key_policy" {
+  template = file("${path.module}/kms-key-policy.json.tpl")
+
+  vars = {
+    arn_root = local.arn_root
+    arn_user = local.arn_user
+  }
+}
+
+# Create KMS Key for encrypting the mail.
+resource "aws_kms_key" "a" {
+  description = "KMS key to use for encrypting exported mail messages"
+
+  deletion_window_in_days = 7
+  policy                  = data.template_file.kms_key_policy.rendered
+}
+
+# Create alias for the KMS key.
+resource "aws_kms_alias" "a" {
+  name          = "alias/mail-export-key"
+  target_key_id = aws_kms_key.a.key_id
 }
 
 # The random string needed for injecting randomness in the name of the S3 bucket.
